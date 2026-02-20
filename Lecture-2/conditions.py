eb3268c3a415c87e6f5a0c4de2bee1fc93076644
Shahid <mrshyam0923@gmail.com>
2026-02-20 11:57:44 +0530

    added conditions file

---
+++ Lecture-2/conditions.py
@@ -0,0 +1,8 @@
+age=int(input("enter your age:"))
+
+if(age>=18):
+    print("you can vote.")
+elif(age<=18):
+    print("you cannot apply vote right.")
+else:
+    print("you are so young..go and play.")
